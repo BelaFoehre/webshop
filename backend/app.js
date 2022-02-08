893f6ba915f33a59b47f6ac19d487fdae1eb0d6c
flo443 <florian.kehl19@gmail.com>
2022-02-08 10:37:11 +0100

    setup

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const app = express();
+
+require("./config/database").connect();
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', "*");
@@ -9,6 +11,6 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,14 @@
 const http = require('http');
-const { debug } = require('console');
 const app = require('./app');
-const mongoose = require('mongoose');
+const server = http.createServer(app);
 
 app.set('port', process.env.PORT);
 
-const MONGO_URL = 'mongodb+srv://'
-                    + process.env.DB_USER
-                    + ':' + process.env.DB_PW
-                    + '@cluster0.0q23p.mongodb.net/'
-                    + process.env.DB_NAME
-                    + '?retryWrites=true&w=majority';
 
-const MONGO_OPTIONS = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-};
-
-mongoose.connect(MONGO_URL, MONGO_OPTIONS);
-
-mongoose.connection
-    .once('open', () => console.log('++ Connection to DB established'))
-    .on('error', (error) => console.warn('-- Connection failed: ', error));
+// server listening 
+// server.listen(port, () => {
+//     console.log(`Server running on port ${port}`);
+//   });
 
 const onListening = () => {
   server.address(process.env.HOST);
@@ -30,6 +17,5 @@ const onListening = () => {
   debug("++ Listening on " + bind);
 };
 
-const server = http.createServer(app);
 server.on("listening", onListening);
 server.listen(process.env.PORT);
