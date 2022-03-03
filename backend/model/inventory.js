fc36c58f6d1c78ac64431cc5a9f935832f0a6e70
flo443 <florian.kehl19@gmail.com>
2022-03-03 16:57:59 +0100

    restructure categories

--- backend/model/inventory.js
+++ backend/model/inventory.js
@@ -4,7 +4,11 @@ const inventorySchema = new mongoose.Schema({
   name: { type: String, default: null },
   brand: { type: String, default: null },
 
-  kategories: { type: [String], default: null},
+  category: {
+    main: { type: String, default: null},
+    sub1: { type: String, default: null},
+    sub2: { type: String, default: null},
+  },
   sizes: [{
       name: { type: String },
       price: { type: Number },
--- backend/routes/dev.js
+++ backend/routes/dev.js
@@ -21,7 +21,10 @@ router.post('/popSample', bodyParser, async (req, res) => {
     const invItem = await Inventory.create({
         name: "Item1",
         brand: "brand1",
-        kategories: ["test1", "test2"],
+        category: {
+            main: "main1",
+            sub1: "sub1_1"
+        },
         sizes: [{
             name: 'Large',
             price: 42,
@@ -35,7 +38,10 @@ router.post('/popSample', bodyParser, async (req, res) => {
     },{
         name: "Item2",
         brand: "brand1",
-        kategories: ["test1", "test2"],
+        category: {
+            main: "main1",
+            sub1: "sub1_2"
+        },
         sizes: [{
             name: 'Large',
             price: 42,
@@ -49,7 +55,37 @@ router.post('/popSample', bodyParser, async (req, res) => {
     },{
         name: "Item",
         brand: "brand2",
-        kategories: ["test1", "test2"],
+        category: {
+            main: "main2",
+            sub1: "sub1_1"
+        },
+        sizes: [{
+            name: 'Large',
+            price: 42,
+            qty: 13
+        },{
+            name: 'Small',
+            price: 12,
+            qty: 420
+        }],
+        tags: ["tag1"]
+    }).then((res) => {
+        console.log(res)
+        return res.status(202)
+    }).catch(err => {
+        return res.status(403).send(err)
+    })
+
+})
+
+router.post('/popSample2', bodyParser, async (req, res) => {
+    const invItem = await Inventory.create({
+        name: "Item42",
+        brand: "brand1",
+        category: {
+            main: "main1",
+            sub1: "newsub"
+        },
         sizes: [{
             name: 'Large',
             price: 42,
